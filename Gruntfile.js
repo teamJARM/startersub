d2e6d9ad7590e05894d8c0798794e91c73d14bc0
Anghiolino <atcoster@gmail.com>
2014-03-24 10:40:41 +0100

    images map created and markers copied into it, Index top navigation created.

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,7 @@ module.exports = function (grunt) {
         connect: {
             options: {
                 port: 9000,
-                livereload: 35729,
+                livereload: 35730,
                 // Change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
