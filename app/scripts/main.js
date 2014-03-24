00b3e4da4dcce40acb28d32806ce95cfc90d07bb
Jeremy Granadillo <akakitsu@gmail.com>
2014-03-24 11:27:01 +0100

    Fix indenting errors, tweak jshint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,7 @@
+'use strict';
+/* global google */
 $(document).on('ready', function(){
-      navigator.geolocation.getCurrentPosition(initialize);
+    navigator.geolocation.getCurrentPosition(initialize);
 });
 
 var map,
@@ -8,79 +10,95 @@ var map,
 
 function initialize(location) {
     var container     = document.getElementById('map-canvas'),
-        infoDiv       = document.getElementById('info'),
+        //infoDiv       = document.getElementById('info'),
         currLocation  = new google.maps.LatLng(location.coords.latitude, location.coords.longitude),
         mapOptions    = {
-                          center: currLocation,
-                          zoom: 12,
-                          mapType: google.maps.MapTypeId.ROADMAP
-                        };
+            center: currLocation,
+            zoom: 12,
+            mapType: google.maps.MapTypeId.ROADMAP,
+            styles:[
+                {
+                    stylers:[{hue: '#2796b8'}]
+                },{
+                    featureType: 'road',
+                    elementType: 'labels',
+                    stylers: [{ visibility: 'off' }]
+                },{
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [
+                        {saturation: -40},
+                        {lightness: 40}
+                    ]
+                }
+            ]
+        };
 
-       map = new google.maps.Map(container, mapOptions);
-     //map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(infoDiv);
+    map = new google.maps.Map(container, mapOptions);
+    //map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(infoDiv);
 
     addMarkers();
 }
 
 function addMarkers() {
-  var iconBase      = root + 'markers/';
-  var icons         = {
-          incubators: {
+    var iconBase      = root + 'markers/';
+    var icons         = {
+        incubators: {
             point: new google.maps.LatLng(51.924655, 4.484926),
             icon: iconBase + 'incubator' + typeImage
-          },
-          startups: {
+        },
+        startups: {
             point: new google.maps.LatLng(51.912981, 4.466759),
             icon: iconBase + 'startup' + typeImage
-          },
-          accelerators: {
+        },
+        accelerators: {
             point: new google.maps.LatLng(51.906366, 4.459603),
             icon: iconBase + 'accelerator' + typeImage
-          }
-      }
+        }
+    };
 
-  var markers = [];
+    var markers = [];
 
-      var incubator = new google.maps.Marker({
-          position: icons.incubators.point,
-          icon: icons.incubators.icon,
-          map: map,
-          title: 'Een incubator',
-          draggable: false,
-          optimized: false,
-          visible: true
-      });
+    var incubator = new google.maps.Marker({
+        position: icons.incubators.point,
+        icon: icons.incubators.icon,
+        map: map,
+        title: 'Een incubator',
+        draggable: false,
+        optimized: false,
+        visible: true
+    });
 
-      var startup = new google.maps.Marker({
-          position: icons.startups.point,
-          icon: icons.startups.icon,
-          map: map,
-          title: 'Een startup',
-          draggable: false,
-          optimized: false,
-          visible: true
-      });
+    var startup = new google.maps.Marker({
+        position: icons.startups.point,
+        icon: icons.startups.icon,
+        map: map,
+        title: 'Een startup',
+        draggable: false,
+        optimized: false,
+        visible: true
+    });
 
-      var accelerator = new google.maps.Marker({
-          position: icons.accelerators.point,
-          icon: icons.accelerators.icon,
-          map: map,
-          title: 'Een accelerator',
-          draggable: true,
-          optimized: false,
-          visible: true,
-          url: 'http://www.google.com'
-      });
+    var accelerator = new google.maps.Marker({
+        position: icons.accelerators.point,
+        icon: icons.accelerators.icon,
+        map: map,
+        title: 'Een accelerator',
+        draggable: true,
+        optimized: false,
+        visible: true,
+        url: 'http://www.google.com'
+    });
 
-      markers.push(incubator, startup, accelerator);
+    markers.push(incubator, startup, accelerator);
 
-      addInfo(markers);
+    addInfo(markers);
 }
 
 function addInfo(m){
-      var markers = m;
+    var markers = m;
 
-      $.each(markers, function(i, val){
+    $.each(markers, function(i, val){
         var marker = val,
             url    = '';
 
@@ -89,11 +107,11 @@ function addInfo(m){
         }
 
         var infowindow = new google.maps.InfoWindow({
-          content: marker.title + ' ' +  url
+            content: marker.title + ' ' +  url
         });
 
         google.maps.event.addListener(marker, 'click', function() {
             infowindow.open(map, marker);
         });
-      });
+    });
 }
