b2d79505244b0ef15b896d84174156110bb01ce2
Anghiolino <atcoster@gmail.com>
2014-03-30 16:48:25 +0200

    radar button click added, markers drop animation added,
    website looks good on web and mobile, but not every element is 100%
    responsive tho.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,11 +5,14 @@ $(document).on('ready', function(){
 });
 
 var map,
-    root = window.location,
+    root = window.location.origin,
     typeImage = '.png';
+
 
 function initialize(location) {
     var container     = document.getElementById('map-canvas'),
+        radar         = $('#gmaps').find('.radar'),
+        infoBar       = $('#info-bar'),
         //infoDiv       = document.getElementById('info'),
         currLocation  = new google.maps.LatLng(location.coords.latitude, location.coords.longitude),
         mapOptions    = {
@@ -34,60 +37,62 @@ function initialize(location) {
             ]
         };
 
+
     map = new google.maps.Map(container, mapOptions);
     //map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(infoDiv);
 
-    addMarkers();
+    radar.on('click', function(){
+            radar.fadeOut(500, function(){
+                addMarkers();
+                infoBar.fadeIn(1000);
+            });
+    });
+
 }
 
 function addMarkers() {
-    var iconBase      = root + 'images/markers/';
-    var icons         = {
-        incubators: {
-            point: new google.maps.LatLng(51.924655, 4.484926),
-            icon: iconBase + 'incubator' + typeImage
-        },
-        startups: {
-            point: new google.maps.LatLng(51.912981, 4.466759),
-            icon: iconBase + 'startup' + typeImage
-        },
-        accelerators: {
-            point: new google.maps.LatLng(51.906366, 4.459603),
-            icon: iconBase + 'accelerator' + typeImage
-        }
-    };
+
+    var iconBase      = root + '/images/markers/';
 
     var markers = [];
 
     var incubator = new google.maps.Marker({
-        position: icons.incubators.point,
-        icon: icons.incubators.icon,
+        position: new google.maps.LatLng(51.924655, 4.484926),
+        icon: iconBase + 'incubator' + typeImage,
         map: map,
-        title: 'Een incubator',
+        title: 'Creative Factory',
+        text: 'Hét verzamelpand voor jong, creatieve ondernemers in Rotterdam',
+        url: 'http://www.creativefactory.nl',
+        logo: '',
         draggable: false,
         optimized: false,
-        visible: true
+        visible: true,
+        animation: google.maps.Animation.DROP
     });
 
     var startup = new google.maps.Marker({
-        position: icons.startups.point,
-        icon: icons.startups.icon,
+        position: new google.maps.LatLng(51.912981, 4.466759),
+        icon: iconBase + 'startup' + typeImage,
         map: map,
-        title: 'Een startup',
+        title: 'Hogeschool Rotterdam',
+        url: 'http://www.hro.nl/',
+        text: ' Het onderwijs op Hogeschool Rotterdam is nauw verweven met de stedelijke en regionale ontwikkelingen in de regio Rotterdam. ',
+        logo: '',
         draggable: false,
         optimized: false,
-        visible: true
+        visible: true,
+        animation: google.maps.Animation.DROP
     });
 
     var accelerator = new google.maps.Marker({
-        position: icons.accelerators.point,
-        icon: icons.accelerators.icon,
+        position: new google.maps.LatLng(51.906366, 4.459603),
+        icon: iconBase + 'accelerator' + typeImage,
         map: map,
-        title: 'Een accelerator',
+        title: 'Gemeente Rotterdam',
         draggable: true,
         optimized: false,
         visible: true,
-        url: 'http://www.google.com'
+        animation: google.maps.Animation.DROP
     });
 
     markers.push(incubator, startup, accelerator);
